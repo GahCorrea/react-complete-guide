82cb90403f83cc9e80c3133284335b26dea187b2
Gabriel Corrêa <gabrielcorrea.1706@gmail.com>
2022-05-26 21:08:32 -0300

    New Features (Adding Conditional Retirn Statements, Conditional Content)

--- src/components/Expenses/Expenses.js
+++ src/components/Expenses/Expenses.js
@@ -1,12 +1,12 @@
 import React, { useState } from 'react';
 
-import ExpenseItem from './ExpenseItem';
 import Card from '../UI/Card';
 import ExpenseFilter from './ExpenseFilter';
+import ExpenseList from './ExpenseList';
 import './Expenses.css';
 
 const Expenses = (props) => {
-    const [filteredYear, setFilteredYear] = useState('2020');
+    const [filteredYear, setFilteredYear] = useState(new Date().getFullYear());
 
     const filterChangeHandler = (selectedYear) => {
         setFilteredYear(selectedYear);
@@ -18,17 +18,11 @@ const Expenses = (props) => {
         );
     });
     
-    let expensesContent = <p>No expenses found.</p>;
-    
-    if ({filteredExpenses.length > 0) {
-        expensesContent = filteredExpenses.map((expense => <ExpenseItem key={expense.id} title={expense.title} amount={expense.amount} date={expense.date} />))
-    }
-
     return (
         <div>
             <Card className='expenses'>
                 <ExpenseFilter selected={filteredYear} onChangeFilter={filterChangeHandler} />
-                {expensesContent}
+                <ExpenseList items={filteredExpenses}/>
             </Card>
         </div>
     );
